d17774038f30cdca0eb73d1ebceb7cdc880776dd
Monika Marczak <marczakmonika83@gmail.com>
2025-06-09 18:23:34 +0200

    .productsUpdate.

--- watchdog/products.ts
+++ watchdog/products.ts
@@ -26,46 +26,35 @@ export const products = [
         threshold: 49,
     },
     {
-        name: 'SKIN79 Rozjaśniająca ampułka z witaminą C Shine Yuja Ampoule 50ml',
-        url: 'https://skin79-sklep.pl/skin79-rozjasniajaca-ampulka-z-witamina-c-shine-yuja-ampoule-50ml.html',
+        name: 'SKIN79 Ampułka redukująca przebarwienia i niedoskonałości Beblesh Dark Spot Ampoule 50ml',
+        url: 'https://skin79-sklep.pl/skin79-ampulka-redukujaca-przebrawienia-beblesh-dark-spot-ampoule-50ml.html',
         selector: 'div.price span.current',
         threshold: 50,
-    },
-    {
-        name: 'MIZON Przeciwzmarszczkowy krem do twarzy',
-        url: 'https://skin79-sklep.pl/mizon-przeciwzmarszczkowy-krem-do-twarzy-z-kolagenem-intensive-firming-solution-collagen-power-firming-enriched-cream-50ml.html',
-        selector: 'div.price span.current',
-        threshold: 55,
     },
 //--------MIZON---------------------------------------------------------------------------------------------------------
     {
-        name: 'MIZON Przeciwzmarszczkowy krem do twarzy',
-        url: 'https://skin79-sklep.pl/mizon-przeciwzmarszczkowy-krem-do-twarzy-z-kolagenem-intensive-firming-solution-collagen-power-firming-enriched-cream-50ml.html',
+        name: 'MIZON Silnie odżywcza esencja do twarzy ze sfermentowanych drożdży Skin Power Original First Essence 210ml',
+        url: 'https://skin79-sklep.pl/mizon-silnie-odzywcza-esencja-do-twarzy-ze-sfermentowanych-drozdzy-skin-power-original-first-essence-210ml.html',
         selector: 'div.price span.current',
-        threshold: 55,
+        threshold: 80,
     },
     {
-        name: 'MIZON Liftingujące serum do twarzy Hyalugen Le Lift Serum 30ml',
-        url: 'https://skin79-sklep.pl/mizon-liftingujace-serum-do-twarzy-hyalugen-le-lift-serum-30ml.html',
+        name: 'MIZON Serum wzmacniające 7 Vegan Peptide Booster Serum 150ml',
+        url: 'https://skin79-sklep.pl/mizon-7-vegan-peptide-booster-serum-150ml.html',
         selector: 'div.price span.current',
-        threshold: 60,
-    },
-    {
-        name: 'MIZON Nawilżające serum do twarzy z kwasem hialuronowym Original Skin Energy Hyaluronic Acid 100 30ml',
-        url: 'https://skin79-sklep.pl/mizon-nawilzajace-serum-do-twarzy-z-kwasem-hialuronowym-original-skin-energy-hyaluronic-acid-100-30ml.html',
-        selector: 'div.price span.current',
-        threshold: 50,
-    },
-    {
-        name: 'MIZON Przeciwzmarszczkowy krem pod oczy z kolagenem Collagen Power Firming Eye Cream 10ml',
-        url: 'https://skin79-sklep.pl/mizon-przeciwzmarszczkowy-krem-pod-oczy-z-kolagenem-collagen-power-firming-eye-cream-10ml.html',
-        selector: 'div.price span.current',
-        threshold: 20,
+        threshold: 49,
     },
     {
         name: 'MIZON Dwufazowa mgiełka do twarzy Hyalugen Double Layer Mist 100ml',
         url: 'https://skin79-sklep.pl/mizon-dwufazowa-mgielka-do-twarzy-hyalugen-double-layer-mist-100ml.html',
         selector: 'div.price span.current',
         threshold: 60,
+    },
+    //------BENTON-------------------------------------------------------------------------------------------------------
+    {
+        name: 'BENTON Serum do twarzy z bakuchiolem Bakuchiol Serum 35ml',
+        url: 'https://skin79-sklep.pl/benton-serum-do-twarzy-z-bakuchiolem-bakuchiol-serum-35ml.html',
+        selector: 'div.price span.current',
+        threshold: 50,
     },
 ];
